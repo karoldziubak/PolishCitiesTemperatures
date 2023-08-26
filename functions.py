b0689b6da9f744dc1db23ff8b8b3f740e1a13ea4
Karol Dziubak <karoldziubak@gmail.com>
2023-08-26 16:27:22 +0200

    data transform and calc stream function prep

---
+++ functions.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def calc_hot_days(df:pd.DataFrame,temp:float)->int:
+    # wyciagnac kolumne 'max temp'
+
+    # zrobic liste z zerami i jedynkami, w zaleznosci czy jest powyzej temp, czy nie
+
+    #zliczasz ilość 1 w serii
+    # [1,1,0,0,0,1,1,1,1,0]
+    # [1,2,0,0,0,1,2,3,4,0]
+
+    #zwracasz największą liczbę z drugiej listy
+    return 0
+
