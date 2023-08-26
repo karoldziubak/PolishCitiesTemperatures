f49939e9b8ec0f5ab9074cab6116a4bcc1a75095
EwaKlajmon <ewa.klajmon@op.pl>
2023-08-26 17:15:24 +0200

    functions

--- functions.py
+++ functions.py
@@ -2,13 +2,21 @@ import pandas as pd
 
 def calc_hot_days(df:pd.DataFrame,temp:float)->int:
     # wyciagnac kolumne 'max temp'
+    temps = df.temp_max.to_list()
 
     # zrobic liste z zerami i jedynkami, w zaleznosci czy jest powyzej temp, czy nie
+    temps_above_threshold = [1 if t > temp else 0 for t in temps]
 
     #zliczasz ilość 1 w serii
     # [1,1,0,0,0,1,1,1,1,0]
     # [1,2,0,0,0,1,2,3,4,0]
+    temps_cumsum = [temps_above_threshold[0]]
+    for i in range(1,len(temps_above_threshold)):
+        if temps_above_threshold[i]==0:
+            temps_cumsum.append(0)
+        elif temps_above_threshold[i]==1:
+            temps_cumsum.append(temps_cumsum[i-1]+1)
 
     #zwracasz największą liczbę z drugiej listy
-    return 0
+    return max(temps_cumsum)
 
